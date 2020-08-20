d3c3ba11adcd338d704313b3458c38c9add2561a
Preston Young <prestonyoung@Prestons-MacBook-Pro.local>
2020-08-19 18:23:54 -0700

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Navigation = () => {
+    return (
+        <nav style={{ display:'flex', justifyContent:'flex-end' }}>
+            <p className="f3 link pa3 dim black underline pointer">Sign out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navigation from '../components/Navigation/Navigation';
+import Logo from '../components/Logo/Logo';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
