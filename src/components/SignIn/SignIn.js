2201f27a7e2ff81a3f81bb66aa7a215a1c98fe8d
Preston Young <prestonyoung@Prestons-MBP.attlocal.net>
2020-08-26 17:06:53 -0700

    Register form working and communicating with back end server

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -28,15 +28,14 @@ class SignIn extends Component {
             })
         })
         .then(response => response.json())
-        .then(data => {
-            if (data === 'Success') {
+        .then(user => {
+            if (user.id) {
                 this.props.onRouteChange('home');
             }
         })
     }
 
     render() {
-        const {onRouteChange} = this.props;
         return (
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
@@ -73,7 +72,7 @@ class SignIn extends Component {
                         </div>
                         <div className="lh-copy mt3">
                             <p 
-                            onClick={() => onRouteChange('register')} 
+                            onClick={() => this.props.onRouteChange('register')} 
                             className="f6 link dim black db pointer"
                             >Register</p>
                         </div>
