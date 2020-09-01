3f5b4b4ed2c7bc5aeaeace4fe92395b310329487
Preston Young <prestonyoung@Prestons-MBP.attlocal.net>
2020-08-31 18:41:40 -0700

    Changed fetch urls to heroku root

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -30,7 +30,7 @@ class Register extends Component{
 
     onSubmitRegister = () => {
         if (this.state.password === this.state.confirmPassword) {
-            fetch('http://localhost:3000/register', {
+            fetch('https://stormy-caverns-07046.herokuapp.com/register', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
